a3ef5042acb3714bbf8e05c610305d1d2a2708d3
Edgaras <baroniunas@gmail.com>
2020-05-07 17:36:46 +0300

    Updates

---
+++ precache-manifest.189f033691f1366d8e6c266206785bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d71b32359aa71c5976fb829ac58bad",
+    "url": "/react-todoList/index.html"
+  },
+  {
+    "revision": "266252a35e9577a9cfcd",
+    "url": "/react-todoList/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "b6032a8f82994decb86e",
+    "url": "/react-todoList/static/css/main.84c52837.chunk.css"
+  },
+  {
+    "revision": "266252a35e9577a9cfcd",
+    "url": "/react-todoList/static/js/2.4646d693.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoList/static/js/2.4646d693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6032a8f82994decb86e",
+    "url": "/react-todoList/static/js/main.eba4ccc0.chunk.js"
+  },
+  {
+    "revision": "906d3390253b279fd8b0",
+    "url": "/react-todoList/static/js/runtime-main.d90af3aa.js"
+  }
+]);
